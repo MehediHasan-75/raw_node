f62bc01f889e255ccefd7980df6f3588dc3efebe
Mehedi Hasan <mehedihasan@Mehedis-MacBook-Air-3.local>
2025-09-28 18:23:05 +0600

    put user

--- handlers/routeHandlers/userHandler.js
+++ handlers/routeHandlers/userHandler.js
@@ -2,6 +2,7 @@
 const data = require('../../lib/data');
 const {hash} = require('../../helpers/utilities');
 const utilities = require('../../helpers/utilities');
+const { user } = require('../../routes');
 //module scaffolding
 const handler = {};
 handler.userHandler = (requestProperties, callback) => {
@@ -91,7 +92,58 @@ handler._users.get = (requestProperties, callback) => {
 };
 
 handler._users.put = (requestProperties, callback) => {
+    //validation
+    const firstName = typeof(requestProperties.body.firstName) === 'string' && requestProperties.body.firstName.trim().length > 0 ? requestProperties.body.firstName : false;
+    const lastName = typeof(requestProperties.body.lastName) === 'string' && requestProperties.body.lastName.trim().length > 0 ? requestProperties.body.lastName : false;
+    const phone = typeof(requestProperties.body.phone) === 'string' && requestProperties.body.phone.trim().length > 0 ? requestProperties.body.phone : false;
+    const password = typeof(requestProperties.body.password) === 'string' && requestProperties.body.password.trim().length > 0 ? requestProperties.body.password : false;
+    
+    if(phone){
+        if(firstName || lastName || password){
+            data.read('users', phone, (err, user ) => {
+                userData = utilities.parseJSON(user);
+                if(!err && userData){
+                    if(firstName){
+                        userData.firstName = firstName;
+                    }
+                    if(lastName){
+                        userData.lastName = lastName;
+                    }
+                    if(password){
+                        userData.password = hash(password);
+                    }
 
+                    data.update('users', phone, userData, (err) => {
+                        if(!err){
+                            callback(200, {
+                                message: "User wash updated successfully!",
+                            });
+                        }
+                        else{
+                            callback(500, {
+                                error: "Error in server side",
+                            });
+                        }
+                    });
+                }
+                else{
+                    callback(400, {
+                        error: "You have a problem in your request!",
+                    });
+                }
+            });
+        }
+        else{
+            callback(400, {
+                error: "You have a problem in your request!",
+            });
+        }
+    }
+    else{
+        callback(400, {
+            error: "Invalid phone number. please try again",
+        })
+    }
 };
 
 handler._users.delete = (requestProperties, callback) => {
