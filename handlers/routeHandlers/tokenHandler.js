e83378ef4019649dbbc5054fd32bc04f66264c41
Mehedi Hasan <mehedihasan@Mehedis-MacBook-Air-3.local>
2025-10-16 17:40:11 +0600

    wip: added post request checking

--- handlers/routeHandlers/tokenHandler.js
+++ handlers/routeHandlers/tokenHandler.js
@@ -5,7 +5,7 @@ Just token is given to the client at first login and it will send token with eve
 //dependencies
 
 const data = require('../../lib/data');
-
+const {hash} = require('../../helpers/utilities')
 
 //module scaffolding
 const handler = {};
@@ -24,7 +24,27 @@ handler.tokenHandler = (requestProperties, callback) => {
 handler._user = {};
 
 handler._user.post = (requestProperties, callback) => {
-
+    const phone = typeof(requestProperties.queryStringObject.phone) === 'string' && requestProperties.queryStringObject.phone.trim().length > 0 ? requestProperties.queryStringObject.phone : false;
+    const password = typeof(requestProperties.body.password) === 'string' && requestProperties.body.password.trim().length > 0 ? requestProperties.body.password : false;
+    //validy of request 
+    if(phone && password){
+        //check if database has this phone and match password?
+        data.read('users', phone, (err, userData)=> {
+            let hashedpassword = hash(password);
+            if(hashedpassword === userData.password){
+            }
+            else{
+                callback(404, {
+                    error: 'password is not valid',
+                });
+            }
+        })
+    }
+    else{
+        callback(404, {
+            error: "You have a problem in your request",
+        });
+    }
 };
 handler._user.get = (requestProperties, callback) => {
 
